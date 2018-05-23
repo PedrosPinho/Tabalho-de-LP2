a92b7e4ca9589dd0f8c71a007cb869baee66c6cb
mateusscm <mateus.miranda@hotmail.com.br>
2018-05-23 13:49:33 -0300

    FuncionarioDAO nao foi no commit passado

---
+++ Trabalgo_LP2/FuncionarioDAO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalgo_LP2
+{
+    class FuncionarioDAO
+    {
+        public void Create(Funcionario f)
+        {
+            Database VannerDB = Database.GetInstance();
+
+            string qry =
+            string.Format("INSERT INTO Funcionario (REGISTRO, NOME, CPF, FUNCAO, DATAINICIO, TELEFONE) VALUES ('{0}','{1}', '{2}', '{3}', '{4}', '{5}')",
+                f.Registro, f.Nome, f.Cpf, f.Funcao, f.Data_inicio, f.Telefone);
+            VannerDB.ExecuteSQL(qry);
+        }
+    }
+}
